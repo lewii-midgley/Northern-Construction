e599dd9179d7ba9afc6901044044bb0d334d9bea
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-06-28 16:21:09 +1200

    Project Complete

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ console.log("sup");
 $("#hamburger").click(  function() {
     $("#nav-mobile").toggle();
 });
+
+const submit = document.getElementById('submit-btn');
+const modal = document.getElementById('contact-modal');
+
+submit.addEventListener('click', () => {
+    modal.classList.add('show');
+});
+
+
+
+
+
